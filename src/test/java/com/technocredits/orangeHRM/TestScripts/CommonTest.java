6b983a5818237156467d08caa50a2edd22670d24
alishachhabra <alisha.chhabra@gmail.com>
2018-10-20 16:10:17 +0530

    Added config, util, constant and pages package. Updated code for LoginPage and added Login test

--- src/test/java/com/technocredits/orangeHRM/TestScripts/CommonTest.java
+++ src/test/java/com/technocredits/orangeHRM/TestScripts/CommonTest.java
@@ -1,12 +1,21 @@
 package com.technocredits.orangeHRM.TestScripts;
 
+import java.io.IOException;
+
+import com.technocredits.orangeHRM.base.PredefinedMethods;
 import com.technocredits.orangeHRM.pages.LoginPage;
 
-public class CommonTest {
 
-	void login(String username, String password) {
-		LoginPage loginPage = LoginPage.getInstance();
-		loginPage.enterValueForLoginPage(username, password);
+public class CommonTest extends PredefinedMethods{
+
+	void login(String username,String password) throws IOException{
+		LoginPage loginpageInstance = LoginPage.getInstance();
+		loginpageInstance.enterValueForLoginPage("usernameTextfield", username);
+		loginpageInstance.enterValueForLoginPage("passwordTextfield", password);
+		loginpageInstance.clickOnLoginPage("loginButton");
+		
+		
 	}
-
+	
+	
 }
--- src/test/java/com/technocredits/orangeHRM/TestScripts/LoginTest.java
+++ src/test/java/com/technocredits/orangeHRM/TestScripts/LoginTest.java
@@ -1,5 +1,15 @@
 package com.technocredits.orangeHRM.TestScripts;
 
-public class LoginTest extends CommonTest {
+import java.io.IOException;
 
+import org.testng.annotations.Test;
+
+
+public class LoginTest extends CommonTest{
+
+	@Test
+	public void doLogin() throws IOException{
+		login("Admin","admin123");
+		System.out.println("updates");
+	}
 }
